e7d296e5e735fea027492337bce7c0c9c6668797
yosuke-76 <85981665+yosuke-76@users.noreply.github.com>
2021-07-27 15:15:38 +0900

    スケジュール投稿機能実装

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,4 +1,26 @@
 class SchedulesController < ApplicationController
+
   def index
+    @schedules = Schedule.all
+  end
+
+  def new
+    @Schedule = Schedule.new
+  end
+
+  def create
+    @schedule = Schedule.new(schedule_params)
+    if @schedule.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+
+
+  private
+  def schedule_params
+    params.permit(:title, :content, :start_time, :end_time).merge(user_id: current_user.id)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_26_052315) do
+ActiveRecord::Schema.define(version: 2021_07_27_031851) do
+
+  create_table "schedules", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "content", null: false
+    t.datetime "start_time", null: false
+    t.datetime "end_time", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_schedules_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -25,4 +36,5 @@ ActiveRecord::Schema.define(version: 2021_07_26_052315) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "schedules", "users"
 end
